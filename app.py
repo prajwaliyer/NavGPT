d16f79589d59639fa500f92538a5f1f26ec85d3b
Rahul Dogiparthy <96254481+Rahuldogiparthy@users.noreply.github.com>
2023-04-29 14:17:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,101 @@ import tiktoken
 import os
 from openai.embeddings_utils import get_embedding
 from dotenv import load_dotenv
+from google.colab import auth
+auth.authenticate_user()
+from youtube_transcript_api import YouTubeTranscriptApi
+import googleapiclient.discovery
+import json
+from google.oauth2 import service_account
+import google.auth.transport.requests
+import pandas as pd
+import requests
 
+#print(requests.get('https://api.ipify.org').text)
+
+API_KEY = "AIzaSyDCtUOqvJB9cEhsHPGKUtQSdjQg4zq8oC8"
+ACCESS_TOKEN = "428637118276-8n95ohv3clke0hj3bdd4k5b7hgs72qr6.apps.googleusercontent.com"
+
+credentials = service_account.Credentials.from_service_account_file("/content/youtubecaptions-384401-fe74ae73c098.json", scopes=["https://www.googleapis.com/auth/youtube.readonly","https://www.googleapis.com/auth/youtube.force-ssl"])
+
+youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=API_KEY, credentials=credentials)
+session = google.auth.transport.requests.AuthorizedSession(credentials)
+
+session.headers.update({'Authorization': 'Bearer ' + ACCESS_TOKEN})
+
+def combine_text_by_duration(data, segment_duration):
+    combined_text = []
+    current_text = ""
+    current_duration = 0
+    for item in data:
+  
+        item_duration = item['duration']
+        item_text = item['text']
+        
+        while item_duration > 0:
+            dcd = segment_duration - current_duration
+            if dcd == segment_duration:
+                item_start = item['start']
+
+            time_to_add = min(dcd, item_duration)
+            current_text += item_text + ' '
+            item_duration -= time_to_add
+            current_duration += time_to_add
+            
+            if current_duration == segment_duration:
+                combined_text.append({'text': current_text, 'start': item_start})
+                fulltext.append(current_text)
+                current_text = ""
+                current_duration = 0
+        
+    return combined_text
+
+def create_df(query):
+    
+    search_response = youtube.search().list(
+    q=query,
+    type="video",
+    part="id,snippet",
+    fields="items(id,snippet)",
+    maxResults=10,
+    ).execute()
+    transcript=[]
+    thumbnail=[]
+    for video_result in search_response["items"]:
+        video_id = video_result["id"]["videoId"]
+        try:
+            transcript.append(YouTubeTranscriptApi.get_transcript(video_id))
+            #print(transcript)
+        except:
+              continue
+    fulltext = []
+
+    for i in range(len(transcript)):
+        combined_text = ''
+        for item in transcript[i]:
+            combined_text += item['text'] + ' '
+        fulltext.append([combined_text])
+    df_ft = pd.DataFrame(fulltext)
+    df_ft.columns=['Full Text']
+    df_ft.head()     
+    y=[]
+    for i in range(len(transcript)):
+        y.append(combine_text_by_duration(transcript[i],180))
+    df_parts = pd.DataFrame(y)
+    data = {'Channel_title': [], 'Title': [], 'Link': [], 'Thumbnail': []}
+    for item in search_response["items"]:
+       title = item['snippet']['title']
+       channel_title = item['snippet']['channelTitle']
+       video_link = "https://www.youtube.com/watch?v=" + item["id"]["videoId"]
+       tn = video_result["snippet"]["thumbnails"]["high"]["url"]
+       data['Link'].append(video_link)
+       data['Title'].append(title)
+       data['Channel_title'].append(channel_title)
+       data['Thumbnail'].append(tn)
+   df_meta = pd.DataFrame(data)
+   df = pd.concat([df_meta,df_ft,df_parts],axis = 1) 
+   df = df.dropna(subset = 'Full Text') 
+    
 load_dotenv()
 openai.organization = os.getenv("ORG_KEY")
 openai.api_key = os.getenv("API_KEY")
